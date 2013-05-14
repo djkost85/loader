660ee86b82599e546866dd28d510c439b8eeda6b
Evgeny Pynykh <bpteam22@gmail.com>
2013-05-14 03:50:48 +0300

    lib_parser-41 poststar.ru

---
+++ proxy_files/proxy_site_module/poststar.ru.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: EC
+ * Date: 14.05.13
+ * Time: 2:36
+ * Project: get_content
+ * @author: Evgeny Pynykh bpteam22@gmail.com
+ */
+namespace poststar;
+use get_content\c_get_content\c_get_content as c_get_content;
+use get_content\c_string_work\c_string_work as c_string_work;
+
+$url_source="http://www.poststar.ru/proxy.htm";
+$name_source="poststar.ru";
+$get_poststar_content= new c_get_content();
+$get_poststar_content->set_type_content("html");
+$answer_poststar=$get_poststar_content->get_content($url_source);
+if(!$answer_poststar) return array();
+if(!$answer_poststar=c_string_work::between_tag($answer_poststar,'<table width="730" border="0" align="center">')) return array();
+if(!preg_match_all("#(?<ip>\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:{1}\d{1,10})\s*)#ims",$answer_poststar,$matches_poststar)) return array();
+$proxy_poststar_proxy=array();
+foreach ($matches_poststar['ip'] as $value_poststar)
+{
+    $tmp_array['proxy']=trim($value_poststar);
+    $tmp_array["source_proxy"]=$name_source;
+    $tmp_array["type_proxy"]='http';
+    $proxy_poststar_proxy['content'][]=$tmp_array;
+}
+return $proxy_poststar_proxy;
