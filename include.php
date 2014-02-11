76bd275f0be5e21dafba39983f20f148ff74197e
bpteam <bpteam22@gmail.com>
2014-02-11 15:06:18 +0200

    fix include
    many define

--- include.php
+++ include.php
@@ -7,17 +7,16 @@
  * Project: get_content
  * @author: Evgeny Pynykh bpteam22@gmail.com
  */
-define('ROOT_DIR', dirname(__FILE__));
-require_once ROOT_DIR . "/cnfg.php";
-require_once ROOT_DIR . "/src/cFile/cFile.php";
-require_once ROOT_DIR . "/src/cList/cList.php";
-require_once ROOT_DIR . "/src/cCookie/cCookie.php";
-require_once ROOT_DIR . "/src/cStringWork/cStringWork.php";
-require_once ROOT_DIR . "/src/cGetContent/cGetContent.php";
-require_once ROOT_DIR . "/src/cCurl/cCurl.php";
-require_once ROOT_DIR . "/src/cCurl/cSingleCurl.php";
-require_once ROOT_DIR . "/src/cCurl/cMultiCurl.php";
-require_once ROOT_DIR . "/src/cProxy/cProxy.php";
-require_once ROOT_DIR . "/src/cProxy/cUpdateProxy.php";
-require_once ROOT_DIR . "/src/cPhantomJS/cPhantomJS.php";
-require_once ROOT_DIR . "/src/cUserAgent/cUserAgent.php";
+require_once dirname(__FILE__) . "/cnfg.php";
+require_once dirname(__FILE__) . "/src/cFile/cFile.php";
+require_once dirname(__FILE__) . "/src/cList/cList.php";
+require_once dirname(__FILE__) . "/src/cCookie/cCookie.php";
+require_once dirname(__FILE__) . "/src/cStringWork/cStringWork.php";
+require_once dirname(__FILE__) . "/src/cGetContent/cGetContent.php";
+require_once dirname(__FILE__) . "/src/cCurl/cCurl.php";
+require_once dirname(__FILE__) . "/src/cCurl/cSingleCurl.php";
+require_once dirname(__FILE__) . "/src/cCurl/cMultiCurl.php";
+require_once dirname(__FILE__) . "/src/cProxy/cProxy.php";
+require_once dirname(__FILE__) . "/src/cProxy/cUpdateProxy.php";
+require_once dirname(__FILE__) . "/src/cPhantomJS/cPhantomJS.php";
+require_once dirname(__FILE__) . "/src/cUserAgent/cUserAgent.php";
