7caafa6319a0e3e55f36f81065c91002f69b39c3
bpteam <bpteam22@gmail.com>
2014-03-03 07:58:30 +0200

    statistic update proxy

--- src/cProxy/support/proxy_update.php
+++ src/cProxy/support/proxy_update.php
@@ -17,11 +17,15 @@ $proxy= new cUpdateProxy('http://free-lance.dyndns.info/proxy_check.php');
 $proxy->updateAllList(true);
 $end = time();
 $text = "\n";
+$proxy->selectList($proxy->getDefaultListName());
+$list = $proxy->getList();
+$subject = count($list['content']);
 foreach($proxy->getAllNameList() as $nameList){
 	$proxy->selectList($nameList);
 	$list = $proxy->getList();
 	$text .= "$nameList " . count($list['content']) . "\n";
 }
 echo date('[H:i:s Y/m/d]', $end);
-echo $echo = $time = round(($end-$start)/60)." m  $text";
-mail("zking.nothingz@gmail.com", "update proxy $time", $echo);
+$time = round(($end-$start)/60);
+echo $echo = $time." m  $text";
+mail("zking.nothingz@gmail.com", "update proxy $time m $subject", $echo);
