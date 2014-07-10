47b8cc5069af518cf1c9c56e1a47a23fdfea3c56
bpteam <bpteam22@gmail.com>
2014-07-10 19:44:24 +0300

    add new source

---
+++ src/cProxy/site_source/checkerproxy.net.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: EC
+ * Date: 14.05.13
+ * Time: 2:36
+ * Project: GetContent
+ * @author: Evgeny Pynykh bpteam22@gmail.com
+ */
+
+require_once dirname(__FILE__)."/../../../include.php";
+use GetContent\cSingleCurl as cSingleCurl;
+use GetContent\cStringWork as cStringWork;
+use GetContent\cUpdateProxy as cUpdateProxy;
+
+$urlSource = "http://checkerproxy.net/ru/all_proxy";
+$nameSource = "checkerproxy.net";
+$curl = new cSingleCurl();
+$updateProxy = new cUpdateProxy();
+$curl->setTypeContent("text");
+$answerCheckerProxy = $curl->load($urlSource);
+$proxyCheckerProxy = array();
+if(preg_match_all('#(?<ip>\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:{1}\d{1,10})\s*)#ims', $answerCheckerProxy, $matchesCheckerProxy)){
+	foreach ($matchesCheckerProxy['ip'] as $valueCheckerProxy) {
+		$valueCheckerProxy = trim($valueCheckerProxy);
+		if(cStringWork::isIp($valueCheckerProxy)){
+			$proxyCheckerProxy[] = $valueCheckerProxy;
+		}
+	}
+}
+$updateProxy->saveSource($nameSource, $proxyCheckerProxy);
+return $proxyCheckerProxy;
